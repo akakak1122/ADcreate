20f470df5d94e011682b2481b715bf5caf7072ff
akakak1122 <3yearswait@naver.com>
2022-03-28 16:26:29 +0900

    feat: add request-ip module

--- src/middlewares/ip.middleware.ts
+++ src/middlewares/ip.middleware.ts
@@ -1,11 +1,12 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { getClientIp } from 'request-ip';
 
 @Injectable()
 export class IPMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request...');
-    console.log(req.headers)
+    const clientIp = getClientIp(req);
+    console.log('ip', clientIp);
     next();
   }
 }
