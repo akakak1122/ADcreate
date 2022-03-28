a5b25c44f11520ac1c8d3fee0cbabea1dfc9ee63
akakak1122 <3yearswait@naver.com>
2022-03-28 18:10:19 +0900

    change: get ip by express request

--- src/middlewares/ip.middleware.ts
+++ src/middlewares/ip.middleware.ts
@@ -1,6 +1,5 @@
 import { HttpException, Injectable, NestMiddleware, HttpStatus } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import { getClientIp } from 'request-ip';
 import { CachingService } from '../transaction/caching';
 
 @Injectable()
@@ -8,7 +7,7 @@ export class IPMiddleware implements NestMiddleware {
   constructor(private cacheManager: CachingService) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
-    const clientIp = getClientIp(req).split(':').pop();
+    const clientIp = (String)(req.headers['x-forwarded-for'] || req.socket.remoteAddress).split(':').pop();
     const blacked = await this.cacheManager.get(`Black:${clientIp}`);
     if (blacked) {
       throw new HttpException({
